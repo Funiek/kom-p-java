e909d40fcd6720a5474eb320925d06de0fdfe728
dawiddmitruk <adres.do.korespondencji@o2.pl>
2022-06-22 18:49:10 +0200

    dziala

--- src/main/java/com/ds360/config/JpaConfig.java
+++ src/main/java/com/ds360/config/JpaConfig.java
@@ -3,30 +3,32 @@ package com.ds360.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.jta.JtaTransactionManager;
 
-import javax.persistence.EntityManagerFactory;
-
 @Configuration
-@EnableJpaRepositories(basePackages = {"com.ds360.komp.repository"})
+@EnableJpaRepositories(basePackages = {"com.ds360.komp.repository"} ,enableDefaultTransactions = false)
+@EnableTransactionManagement
 public class JpaConfig {
+    
     @Bean
     public LocalEntityManagerFactoryBean entityManagerFactory() {
         LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
         factoryBean.setPersistenceUnitName("KompDB");
-
+        
         return factoryBean;
     }
     
     @Bean
-    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory);
-        
+    public JtaTransactionManager transactionManager() {
+        var transactionManager = new JtaTransactionManager();
+        transactionManager.setAutodetectUserTransaction(true);
+        transactionManager.setNestedTransactionAllowed(true);
+        transactionManager.setDefaultTimeout(100);
+        transactionManager.setRollbackOnCommitFailure(false);
+        transactionManager.setGlobalRollbackOnParticipationFailure(false);
         return transactionManager;
     }
-    
     
 }
